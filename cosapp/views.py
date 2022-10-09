89dd23687ac3f41ba9a1d4dbb980fed0acb3973b
RosanaNei <rosageene@gmail.com>
2022-10-09 13:59:35 -0300

    empezando coso

---
+++ cosapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'cosapp/index.html', {'hoy': datetime.now})
+
+
